c3109a127334940cbb2f9c2878790869c95cca21
Vitarico <victoria.hauzeneder@gmail.com>
2022-05-26 22:22:49 +0200

    Link to FAQ in Mint Flow

--- src/messages/connectedMessage.ts
+++ src/messages/connectedMessage.ts
@@ -212,11 +212,14 @@ export const mainMenuMessage: MessageContent = {
         genesisPretzelContext
       ) => {
         let address = web3Context.address
-        let newHist = await messageContext.addMessage({
-          content: 'Genesis Pretzels sounds interesting!',
-          type: MessageType.text,
-          sendByUser: true,
-        },[])
+        let newHist = await messageContext.addMessage(
+          {
+            content: 'Genesis Pretzels sounds interesting!',
+            type: MessageType.text,
+            sendByUser: true,
+          },
+          []
+        )
         changeToSecret(messageContext)
         web3Context.setTargetContract('GENESIS_PRETZEL_CONTRACT')
         console.log('ON CHAIN:', web3Context.targetContract)
@@ -697,6 +700,28 @@ export const firstFreePretzelMessage: MessageContent = {
         return messageContext.addMessage(mainMenuMessage, newHist)
       },
     },
+    {
+      content: 'Link to FAQ',
+      onClick: async (messageContext, web3Context) => {
+        let newHist = await messageContext.addMessage({
+          content: 'I would like to learn more.',
+          type: MessageType.text,
+          sendByUser: true,
+        })
+        const url =
+          'https://www.notion.so/pretzeldao/The-Bakery-FAQ-9324e4ace9a948b681ec994b50d133a4'
+        const newWindow = window.open(url, '_blank', 'noopener,noreferrer')
+        if (newWindow) newWindow.opener = null
+        newHist = await messageContext.addMessage(
+          {
+            content: 'Sure, let me know when you are ready!',
+            type: MessageType.text,
+          },
+          newHist
+        )
+        return messageContext.addMessage(mainMenuMessage, newHist)
+      },
+    },
   ],
   delay: 1000,
   type: MessageType.text,
@@ -720,7 +745,6 @@ export const freePretzelMessage: MessageContent = {
         if (web3Context.isCorrectChain()) {
           console.log('trying to mint now')
           console.log(sugarPretzelContext)
-          
 
           const tokenIdPromise = sugarPretzelContext.mintSugarPretzel()
 
@@ -738,10 +762,9 @@ export const freePretzelMessage: MessageContent = {
             },
             newHist
           )
-          console.log("awaiting id")
+          console.log('awaiting id')
           const tokenId = await tokenIdPromise
-          console.log("got id",tokenId)
-
+          console.log('got id', tokenId)
 
           //TODO @Johannes spinning wheel?
 
@@ -753,10 +776,10 @@ export const freePretzelMessage: MessageContent = {
               newHist
             )
           } else {
-            console.log("fetching ",tokenId)
+            console.log('fetching ', tokenId)
             const data = await fetch(CONFIG.BACKEND_URL + '/bakery/' + tokenId)
             const datajson = await data.json()
-            console.log("adding image for ",datajson)
+            console.log('adding image for ', datajson)
             newHist = await messageContext.addMessage(
               {
                 content: 'Look at this fantastic Pretzel:',
@@ -789,6 +812,28 @@ export const freePretzelMessage: MessageContent = {
           type: MessageType.text,
           sendByUser: true,
         })
+        return messageContext.addMessage(mainMenuMessage, newHist)
+      },
+    },
+    {
+      content: 'Link to FAQ',
+      onClick: async (messageContext, web3Context) => {
+        let newHist = await messageContext.addMessage({
+          content: 'I would like to learn more.',
+          type: MessageType.text,
+          sendByUser: true,
+        })
+        const url =
+          'https://www.notion.so/pretzeldao/The-Bakery-FAQ-9324e4ace9a948b681ec994b50d133a4'
+        const newWindow = window.open(url, '_blank', 'noopener,noreferrer')
+        if (newWindow) newWindow.opener = null
+        newHist = await messageContext.addMessage(
+          {
+            content: 'Sure, let me know when you are ready!',
+            type: MessageType.text,
+          },
+          newHist
+        )
         return messageContext.addMessage(mainMenuMessage, newHist)
       },
     },
@@ -1221,6 +1266,29 @@ export const genesisPretzelMessage1: MessageContent = {
           3,
           newHist
         )
+      },
+    },
+    {
+      content: 'Link to FAQ',
+      onClick: async (messageContext, web3Context) => {
+        let newHist = await messageContext.addMessage({
+          content: 'I would like to learn more.',
+          type: MessageType.text,
+          sendByUser: true,
+        })
+        const url =
+          'https://www.notion.so/pretzeldao/The-Bakery-FAQ-9324e4ace9a948b681ec994b50d133a4'
+        const newWindow = window.open(url, '_blank', 'noopener,noreferrer')
+        if (newWindow) newWindow.opener = null
+        newHist = await messageContext.addMessage(
+          {
+            content: 'Sure, let me know when you are ready!',
+            type: MessageType.text,
+          },
+          newHist
+        )
+        changeToInside(messageContext)
+        return messageContext.addMessage(mainMenuMessage, newHist)
       },
     },
     {
