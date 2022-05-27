026f894b3ed9dab755aae21ed53eb69bc5345943
Nick Stracke <nick.stracke@web.de>
2022-05-27 08:36:29 +0200

    Revert "Merge pull request #74 from PretzelDAO/polygon-swp-fix"
    This reverts commit 6be2441d9510a0bd929a09ac1c8332f6e9ef95f5, reversing
    changes made to 5ce93b3e44ea600dc33627f5c5c9d189b52ccba7.

--- src/messages/connectedMessage.ts
+++ src/messages/connectedMessage.ts
@@ -596,10 +596,13 @@ export const changeChainPolygonMessage: MessageContent = {
         //TODO @Nick @Johannes fix because not reliable!
         const switchSuccess = await web3Context.switchToCorrectChain()
         if (!switchSuccess) {
-          return messageContext.addMessage(recheckChainPolygonMessage, newHist)
-        }else{
-          return messageContext.addMessage(recheckChainPolygonMessage, newHist)
-
+          return messageContext.addMessage(changeChainPolygonMessage, newHist)
+        }
+        const _canMintGasless = await ISugarPretzelContext.canMintGasless()
+        if (_canMintGasless) {
+          return messageContext.addMessage(firstFreePretzelMessage, newHist)
+        } else {
+          return messageContext.addMessage(freePretzelMessage, newHist)
         }
       },
     },
@@ -630,50 +633,6 @@ export const changeChainPolygonMessage: MessageContent = {
   type: MessageType.text,
 }
 
-
-export const recheckChainPolygonMessage: MessageContent = {
-  content: [
-    'I will check again if the chain is correct.',
-  ],
-  actions: [
-    {
-      content: 'OK!',
-      onClick: async (messageContext, web3Context, ISugarPretzelContext) => {
-        let newHist = await messageContext.addMessage({
-          content: 'Changing to Polygon',
-          type: MessageType.text,
-          sendByUser: true,
-        })
-        //TODO @Nick @Johannes fix because not reliable!
-        //always introduce the second message fro context updates
-        const rightChain = web3Context.isCorrectChain()
-        if (!rightChain) {
-          return messageContext.addMessage(changeChainPolygonMessage, newHist)
-        }
-        const _canMintGasless = await ISugarPretzelContext.canMintGasless()
-        if (_canMintGasless) {
-          return messageContext.addMessage(firstFreePretzelMessage, newHist)
-        } else {
-          return messageContext.addMessage(freePretzelMessage, newHist)
-        }
-      },
-    },
-    {
-      content: 'Never',
-      onClick: async (messageContext) => {
-        const newHist = await messageContext.addMessage({
-          content: 'I do not want a Pretzel',
-          type: MessageType.text,
-          sendByUser: true,
-        })
-        return messageContext.addMessage(mainMenuMessage, newHist)
-      },
-    },
-  ],
-  delay: 1000,
-  type: MessageType.text,
-}
-
 export const changeChainPolygonMessage2: MessageContent = {
   content: ['Great, now let us switch Chain'],
   actions: [
@@ -686,8 +645,8 @@ export const changeChainPolygonMessage2: MessageContent = {
           sendByUser: true,
         })
         //TODO @Nick @Johannes fix because not reliable!
-        const isCorrectChain =  web3Context.isCorrectChain()
-        if (!isCorrectChain) {
+        const switchSuccess = await web3Context.switchToCorrectChain()
+        if (!switchSuccess) {
           return messageContext.addMessage(changeChainPolygonMessage, newHist)
         }
         const _canMintGasless = await ISugarPretzelContext.canMintGasless()
